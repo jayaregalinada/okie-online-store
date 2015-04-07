50c70896b303cc291ec2a98cf26b2372d3d467dc
Jay Are Galinada <jayaregalinada@gmail.com>
2015-04-08 05:36:59 +0800

    Move AuthenticateUser to services

--- app/Services/User/AuthenticateUser.php
+++ app/Services/User/AuthenticateUser.php
@@ -1,16 +1,33 @@
-<?php namespace Okie;
+<?php namespace Okie\Services\User;
 
 use Illuminate\Contracts\Auth\Guard;
 use Laravel\Socialite\Contracts\Factory as Socialite;
 use Okie\Repositories\UserRepository;
-use Request;
 
 class AuthenticateUser {
 
+	/**
+	 * @var \Laravel\Socialite\Contracts\Factory
+	 */
 	private $socialite;
+
+	/**
+	 * @var \Illuminate\Contracts\Auth\Guard
+	 */
 	private $auth;
+
+	/**
+	 * @var \Okie\Repositories\UserRepository
+	 */
 	private $users;
 
+	/**
+	 * Create new instance
+	 *
+	 * @param Socialite      $socialite
+	 * @param Guard          $auth
+	 * @param UserRepository $users
+	 */
 	public function __construct( Socialite $socialite, Guard $auth, UserRepository $users )
 	{
 		$this->socialite = $socialite;
@@ -18,22 +35,39 @@ class AuthenticateUser {
 		$this->auth      = $auth;
 	}
 
+	/**
+	 * Execute request
+	 *
+	 * @param $request
+	 * @param $listener
+	 * @param $provider
+	 *
+	 * @return \Symfony\Component\HttpFoundation\RedirectResponse
+	 */
 	public function execute( $request, $listener, $provider )
 	{
-		if (!$request) return $this->getAuthorizationFirst( $provider );
+		if ( ! $request ) return $this->getAuthorizationFirst( $provider );
 		$user = $this->users->findByUserNameOrCreate( $this->getSocialUser( $provider ), $provider );
-
 		$this->auth->login( $user, true );
 
 		return $listener->userHasLoggedIn( $user );
-		// var_dump( $this->getSocialUser( $provider ) );
 	}
 
+	/**
+	 * @param  string  $provider
+	 *
+	 * @return \Symfony\Component\HttpFoundation\RedirectResponse
+	 */
 	private function getAuthorizationFirst( $provider )
 	{
 		return $this->socialite->driver( $provider )->redirect();
 	}
 
+	/**
+	 * @param  string  $provider
+	 *
+	 * @return \Laravel\Socialite\Contracts\User
+	 */
 	private function getSocialUser( $provider )
 	{
 		return $this->socialite->driver( $provider )->user();
