93c9474d7553f5087bd30d11b67afcebeba26498
Jay Are Galinada <jayaregalinada@gmail.com>
2015-04-08 05:43:25 +0800

    The upload form request

---
+++ app/Http/Requests/UploadFormRequest.php
@@ -0,0 +1,29 @@
+<?php namespace Okie\Http\Requests;
+
+use Okie\Http\Requests\Request;
+
+class UploadFormRequest extends Request {
+
+	/**
+	 * Determine if the user is authorized to make this request.
+	 *
+	 * @return bool
+	 */
+	public function authorize()
+	{
+		return false;
+	}
+
+	/**
+	 * Get the validation rules that apply to the request.
+	 *
+	 * @return array
+	 */
+	public function rules()
+	{
+		return [
+			//
+		];
+	}
+
+}
