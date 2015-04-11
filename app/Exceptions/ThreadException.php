374cc21ab4f86c7f9ecbbbb8a4e98ac256497b96
Jay Are Galinada <jayaregalinada@gmail.com>
2015-04-12 07:17:00 +0800

    More complex ThreadException

--- app/Exceptions/ThreadException.php
+++ app/Exceptions/ThreadException.php
@@ -4,9 +4,58 @@ use Exception;
 
 class ThreadException extends Exception {
 
+	/**
+	 * @type string
+	 */
+	protected $type;
+
+	/**
+	 * @var array
+	 */
+	protected $data;
+
+	/**
+	 * @param string $type
+	 * @param null   $message
+	 * @param int    $code
+	 * @param array  $data
+	 */
+	public function __construct( $type = 'GLOBAL', $message = null, $code = 404, $data = [] )
+	{
+		$this->type = $type;
+		parent::__construct( '['. $type . '] ' . $message, $code );
+	}
+
+	/**
+	 * @return string
+	 */
 	public function getType()
 	{
-		return 'danger';
+		return $this->type;
+	}
+
+	/**
+	 * @return array
+	 */
+	public function getData()
+	{
+		return $this->data;
+	}
+
+	/**
+	 * @return array
+	 */
+	public function showInResponse()
+	{
+		return [
+			'error' => [
+				'message'   => $this->message,
+				'type'      => $this->type,
+				'code'      => $this->code,
+				'exception' => get_class( $this ),
+				'data'      => $this->data
+			]
+		];
 	}
 
 }
