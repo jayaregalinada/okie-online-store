caa727e62683603608349e3154498308e2a83044
Jay Are Galinada <jayaregalinada@gmail.com>
2015-04-12 07:22:19 +0800

    Migration for options and newsletters

---
+++ database/migrations/2015_04_12_022438_create_options_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOptionsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('options', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('type')->nullable();
+			$table->string('key')->nullable();
+			$table->string('value');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('options');
+	}
+
+}
