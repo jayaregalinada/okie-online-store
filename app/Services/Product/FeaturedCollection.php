cbb4140a5371c30124e25b649e13d744f0d0c395
Jay Are Galinada <jayaregalinada@gmail.com>
2015-04-30 15:42:55 +0800

    Collections for Product featured items

--- app/Services/Product/FeaturedCollection.php
+++ app/Services/Product/FeaturedCollection.php
@@ -1,20 +1,25 @@
 <?php namespace Okie\Services\Product;
 
 use Illuminate\Database\Eloquent\Collection;
+use Okie\Exceptions\ProductException;
 
 class FeaturedCollection extends Collection {
 
-	public function getFeatured( $rating, $operator = '>', $limit )
+	public function getFeatured( $rating, $operator = '>', $limit, $count )
 	{
 		$i = [];
-		foreach ($this->items as $key => $value)
+		foreach ( $this->items as $key => $value )
 		{
-			if( $this->rating( $value->getRatingAttribute()['average'], $operator, $rating ) || $value->featured )
+			if( ( $this->rating( $value->getRatingAttribute()['average'], $operator, $rating ) && $value->getRatingAttribute()['count'] > $count ) || $value->featured )
 				$i[] = $value;
 		}
 		shuffle( $i );
 
-		return array_slice( $i, 0, $limit );
+		if( empty( $i ) )
+			return parent::make( [] );
+			
+
+		return parent::make( array_slice( $i, 0, $limit ) );
 	}
 
 	protected function rating( $value, $operator, $compare )
