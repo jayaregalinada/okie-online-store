a7a6f81b1890d35c7e20ead8a785dd25ff7c2cf5
Jay Are Galinada <jayaregalinada@gmail.com>
2015-04-30 15:43:14 +0800

    Configuration for responses

---
+++ config/responses.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+
+	'inquiry' => 'Thank you for inquiring this product. We will get in touch with you soon. Please always check your messages for possible response',
+	'other_items' => 'You might also looking for',
+
+];
+
+ 
