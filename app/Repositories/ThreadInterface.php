85fcf93f226cc0f296f86d822f7732c456fdf07c
Jay Are Galinada <jayaregalinada@gmail.com>
2015-04-30 15:45:23 +0800

    Hello ThreadInterface

--- app/Repositories/ThreadInterface.php
+++ app/Repositories/ThreadInterface.php
@@ -1,11 +1,24 @@
 <?php namespace Okie\Repositories;
-/** TODO: PhpDocs */
+
 interface ThreadInterface {
 
+	/**
+	 * @return mixed
+	 */
 	public function getAll();
 
+	/**
+	 * @param $id
+	 *
+	 * @return mixed
+	 */
 	public function get( $id );
 
+	/**
+	 * @param $id
+	 *
+	 * @return mixed
+	 */
 	public function getConversations( $id );
 
 }
