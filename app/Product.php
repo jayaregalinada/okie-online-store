ca2793350f2c0cd59c2983d6f64426540a2ded6a
Jay Are Galinada <jayaregalinada@gmail.com>
2015-05-05 20:16:44 +0800

    [FIXED] Use PSR-2 indent

--- app/Product.php
+++ app/Product.php
@@ -36,274 +36,275 @@ use Okie\Services\Product\FeaturedCollection;
  */
 class Product extends Model {
 
-	use SoftDeletes;
+    use SoftDeletes;
 
-	/**
-	 * @var string
-	 */
-	protected $table = 'products';
+    /**
+     * @var string
+     */
+    protected $table = 'products';
 
-	/**
-	 * @var array
-	 */
-	protected $fillable = [ 'name', 'code', 'description', 'price', 'unit', 'user_id', 'thumbnail_id', 'badge', 'sale_price', 'featured' ];
+    /**
+     * @var array
+     */
+    protected $fillable = [ 'name', 'code', 'description', 'price', 'unit', 'user_id', 'thumbnail_id', 'badge', 'sale_price', 'featured' ];
 
-	/**
-	 * @var array
-	 */
-	protected $dates = [ 'deleted_at' ];
+    /**
+     * @var array
+     */
+    protected $dates = [ 'deleted_at' ];
 
-	/**
-	 * @var array
-	 */
-	protected $hidden = [ 'user_id', 'created_at', 'deleted_at', 'thumbnail_id' ];
+    /**
+     * @var array
+     */
+    protected $hidden = [ 'user_id', 'created_at', 'deleted_at', 'thumbnail_id' ];
 
-	/**
-	 * @var integer
-	 */
-	protected $perPage = 15;
+    /**
+     * @var integer
+     */
+    protected $perPage = 15;
 
-	/**
-	 * @var array
-	 */
-	protected $appends = [ 'thumbnail', 'rating' ];
+    /**
+     * @var array
+     */
+    protected $appends = [ 'thumbnail', 'rating' ];
 
-	/**
-	 * @var array
-	 */
-	protected $with = [ 'categories', 'images' ];
+    /**
+     * @var array
+     */
+    protected $with = [ 'categories', 'images' ];
 
-	/**
-	 * @type array
-	 */
-	protected $casts = [
-		'id' => 'integer',
-		'unit' => 'integer',
-		'user_id' => 'integer',
-		'thumbnail_id' => 'integer',
-		'featured' => 'boolean'
-	];
+    /**
+     * @type array
+     */
+    protected $casts = [
+        'id' => 'integer',
+        'unit' => 'integer',
+        'user_id' => 'integer',
+        'thumbnail_id' => 'integer',
+        'featured' => 'boolean'
+    ];
 
-	/**
-	 * Possible object key for badge column
-	 * 
-	 * @type array
-	 */
-	public $badgeAttributes = [
-		'title', 'description', 'slug', 'class', 'class_array'
-	];
+    /**
+     * Possible object key for badge column
+     * 
+     * @type array
+     */
+    public $badgeAttributes = [
+        'title', 'description', 'slug', 'class', 'class_array', 'color'
+    ];
 
-	/**
-	 * Images relation
-	 *
-	 * @return \Illuminate\Database\Eloquent\Relations\HasMany
-	 */
-	public function images()
-	{
-		return $this->hasMany( 'Okie\Image' );
-	}
+    /**
+     * Images relation
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function images()
+    {
+        return $this->hasMany( 'Okie\Image' );
+    }
 
-	/**
-	 * Categories relation
-	 *
-	 * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
-	 */
-	public function categories()
-	{
-		return $this->belongsToMany( 'Okie\Category', 'product_category' );
-	}
+    /**
+     * Categories relation
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function categories()
+    {
+        return $this->belongsToMany( 'Okie\Category', 'product_category' );
+    }
 
-	/**
-	 * User relation
-	 *
-	 * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
-	 */
-	public function user()
-	{
-		return $this->belongsTo( 'Okie\User' );
-	}
+    /**
+     * User relation
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo( 'Okie\User' );
+    }
 
-	/**
-	 * Get the Thumbnail
-	 * Check if in column `thumbnail_id` exists in images table
-	 *
-	 * @return object|\Illuminate\Database\Eloquent\Relations\HasMany
-	 */
-	public function getThumbnail()
-	{
-		if( $this->images()->whereId( $this->thumbnail_id )->exists() )
-			return $this->images()->whereId( $this->thumbnail_id )->first();
+    /**
+     * Get the Thumbnail
+     * Check if in column `thumbnail_id` exists in images table
+     *
+     * @return object|\Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function getThumbnail()
+    {
+        if( $this->images()->whereId( $this->thumbnail_id )->exists() )
+            return $this->images()->whereId( $this->thumbnail_id )->first();
 
-		return $this->images->first();
-	}
+        return $this->images->first();
+    }
 
-	/**
-	 * Attributes for `thumbnail`
-	 *
-	 * @return array
-	 */
-	public function getThumbnailAttribute()
-	{
-		if( is_null( $this->getThumbnail()['sizes'] ) )
-			return $this->defaultThumbnail();
+    /**
+     * Attributes for `thumbnail`
+     *
+     * @return array
+     */
+    public function getThumbnailAttribute()
+    {
+        if( is_null( $this->getThumbnail()['sizes'] ) )
+            return $this->defaultThumbnail();
 
-		return $this->getThumbnail()['sizes'];
-	}
+        return $this->getThumbnail()['sizes'];
+    }
 
-	/**
-	 * @return \Illuminate\Database\Eloquent\Relations\HasMany
-	 */
-	public function reviews()
-	{
-		return $this->hasMany( 'Okie\Review' );
-	}
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function reviews()
+    {
+        return $this->hasMany( 'Okie\Review' );
+    }
 
-	/**
-	 * @param $query
-	 *
-	 * @return float
-	 */
-	public function scopeCalculateRating( $query )
-	{
-		$reviews = $this->reviews()->whereNotNull( 'approved_by' );
-		$avgRating = $reviews->avg( 'rating' );
+    /**
+     * @param $query
+     *
+     * @return float
+     */
+    public function scopeCalculateRating( $query )
+    {
+        $reviews = $this->reviews()->whereNotNull( 'approved_by' );
+        $avgRating = $reviews->avg( 'rating' );
 
-		return round( $avgRating, 1 );
-	}
+        return round( $avgRating, 1 );
+    }
 
-	/**
-	 * @return array
-	 */
-	public function getRatingAttribute()
-	{
-		return [
-			'average' => $this->calculateRating(),
-			'count'   => $this->reviews()->whereNotNull( 'approved_by' )->count()
-		];
-	}
+    /**
+     * @return array
+     */
+    public function getRatingAttribute()
+    {
+        return [
+            'average' => $this->calculateRating(),
+            'count'   => $this->reviews()->whereNotNull( 'approved_by' )->count()
+        ];
+    }
 
-	/**
-	 * If product do not have any thumbnail
-	 *
-	 * @return array
-	 */
-	private function defaultThumbnail()
-	{
-		return [
-			[
-				'width' => 600,
-				'height' => 600,
-				'url' => url( '/images/defaults/product_org.jpg' ),
-				'base_dir' => '/images/defaults/product_org.jpg'
-			],
-			[
-				'width' => 50,
-				'height' => 50,
-				'url' => url( '/images/defaults/product_sqr.jpg' ),
-				'base_dir' => '/images/defaults/product_sqr.jpg'
-			],
-			[
-				'width' => 280,
-				'height' => 280,
-				'url' => url( '/images/defaults/product_thn.jpg' ),
-				'base_dir' => '/images/defaults/product_thn.jpg'
-			],
-			[
-				'width' => 150,
-				'height' => 150,
-				'url' => url( '/images/defaults/product_sml.jpg' ),
-				'base_dir' => '/images/defaults/product_sml.jpg'
-			],
-			[
-				'width' => 300,
-				'height' => 300,
-				'url' => url( '/images/defaults/product_mdm.jpg' ),
-				'base_dir' => '/images/defaults/product_mdm.jpg'
-			],
-			[
-				'width' => 600,
-				'height' => 600,
-				'url' => url( '/images/defaults/product_lrg.jpg' ),
-				'base_dir' => '/images/defaults/product_lrg.jpg'
-			]
-		];
-	}
+    /**
+     * If product do not have any thumbnail
+     *
+     * @return array
+     */
+    private function defaultThumbnail()
+    {
+        return [
+            [
+                'width' => 600,
+                'height' => 600,
+                'url' => url( '/images/defaults/product_org.jpg' ),
+                'base_dir' => '/images/defaults/product_org.jpg'
+            ],
+            [
+                'width' => 50,
+                'height' => 50,
+                'url' => url( '/images/defaults/product_sqr.jpg' ),
+                'base_dir' => '/images/defaults/product_sqr.jpg'
+            ],
+            [
+                'width' => 280,
+                'height' => 280,
+                'url' => url( '/images/defaults/product_thn.jpg' ),
+                'base_dir' => '/images/defaults/product_thn.jpg'
+            ],
+            [
+                'width' => 150,
+                'height' => 150,
+                'url' => url( '/images/defaults/product_sml.jpg' ),
+                'base_dir' => '/images/defaults/product_sml.jpg'
+            ],
+            [
+                'width' => 300,
+                'height' => 300,
+                'url' => url( '/images/defaults/product_mdm.jpg' ),
+                'base_dir' => '/images/defaults/product_mdm.jpg'
+            ],
+            [
+                'width' => 600,
+                'height' => 600,
+                'url' => url( '/images/defaults/product_lrg.jpg' ),
+                'base_dir' => '/images/defaults/product_lrg.jpg'
+            ]
+        ];
+    }
 
-	/**
-	 * @param $value
-	 *
-	 * @return null|string
-	 */
-	public function setBadgeAttribute( $value )
-	{
-		if( is_null( $value ) )
-			return $this->attributes[ 'badge' ] = null;
-		else
-			return $this->attributes[ 'badge' ] = serialize( $value );
-	}
+    /**
+     * @param $value
+     *
+     * @return null|string
+     */
+    public function setBadgeAttribute( $value )
+    {
+        if( is_null( $value ) )
+            return $this->attributes[ 'badge' ] = null;
+        else
+            return $this->attributes[ 'badge' ] = serialize( $value );
+    }
 
-	/**
-	 * @param $value
-	 *
-	 * @return array|mixed
-	 */
-	public function getBadgeAttribute( $value )
-	{
-		if( ! ( $this->attributes[ 'badge' ] ) )
-			return [
-				'class' => 'ribbon-default',
-				'class_array' => [ 'ribbon-default' ]
-			];
+    /**
+     * @param $value
+     *
+     * @return array|mixed
+     */
+    public function getBadgeAttribute( $value )
+    {
+        if( ! ( $this->attributes[ 'badge' ] ) )
+            return [
+                'class' => 'ribbon-default',
+                'class_array' => [ 'ribbon-default' ]
+            ];
 
-		return unserialize( $value );
-	}
+        return unserialize( $value );
+    }
 
-	/**
-	 * @param array $data
-	 *
-	 * @return bool|int
-	 */
-	public function editBadge( array $data )
-	{
-		return $this->update( [
-			'badge' => [
-				'title' => $data[ 'title' ],
-				'description' => $data[ 'description' ],
-				'slug' => str_slug( $data[ 'title' ] ),
-				'class' => $data[ 'class' ],
-				'class_array' => explode( ' ', $data[ 'class' ] )
-			]
-		] );
-	}
+    /**
+     * @param array $data
+     *
+     * @return bool|int
+     */
+    public function editBadge( array $data )
+    {
+        return $this->update( [
+            'badge' => [
+                'title'         => $data[ 'title' ],
+                'description'   => $data[ 'description' ],
+                'slug'          => str_slug( $data[ 'title' ] ),
+                'class'         => $data[ 'class' ],
+                'class_array'   => explode( ' ', $data[ 'class' ] ),
+                'color'         => $data[ 'color' ]
+            ]
+        ] );
+    }
 
-	/**
-	 * @return bool|int
-	 */
-	public function destroyBadge()
-	{
-		return $this->update( [
-			'badge' => null ]
-		);
-	}
+    /**
+     * @return bool|int
+     */
+    public function destroyBadge()
+    {
+        return $this->update( [
+            'badge' => null ]
+        );
+    }
 
-	/**
-	 * @param     $query
-	 * @param     $id
-	 * @param int $limit
-	 *
-	 * @return mixed
-	 */
-	public function scopeGetRelated( $query, $id, $limit = 5 )
-	{
-		return $query->where( 'id', '!=', $id )->orderByRaw( "RAND()" )->limit( $limit )->get();
-	}
+    /**
+     * @param     $query
+     * @param     $id
+     * @param int $limit
+     *
+     * @return mixed
+     */
+    public function scopeGetRelated( $query, $id, $limit = 5 )
+    {
+        return $query->where( 'id', '!=', $id )->orderByRaw( "RAND()" )->limit( $limit )->get();
+    }
 
-	public function scopeGetFeatured( $query, $limit = 3, $rating = null, $operator = null, $count = null )
-	{
-		$rating   = ( $rating ) ? $rating : config( 'product.featured.rating' );
-		$operator = ( $operator ) ? $operator : config( 'product.featured.operator' );
-		$count    = ( $count ) ? $count : config( 'product.featured.count' );
-		return ( new FeaturedCollection( $this->all() ) )->getFeatured( $rating, $operator, $limit, $count );
-	}
+    public function scopeGetFeatured( $query, $limit = 3, $rating = null, $operator = null, $count = null )
+    {
+        $rating   = ( $rating ) ? $rating : config( 'product.featured.rating' );
+        $operator = ( $operator ) ? $operator : config( 'product.featured.operator' );
+        $count    = ( $count ) ? $count : config( 'product.featured.count' );
+        return ( new FeaturedCollection( $this->all() ) )->getFeatured( $rating, $operator, $limit, $count );
+    }
 
 }
