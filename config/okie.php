94adcb69df70ca442cc9c9b902c6c4e8f7eed0ad
Jay Are Galinada <jayaregalinada@gmail.com>
2015-07-04 16:21:26 +0800

    Terms and condition

--- config/okie.php
+++ config/okie.php
@@ -7,7 +7,7 @@ return [
 	],
 	'banner' => [
 		'quality' => [
-			'jpg' =>90,
+			'jpg' => 90,
 			'image' => 80
 		],
 		'full_width' => false,
@@ -47,8 +47,63 @@ return [
 	],
 	'terms_and_conditions' => [
 		'url' => 'terms-and-conditions',
-		'contents' => '<p>Insert Terms and Conditions here.</p>'
+		'contents' => '<p>1.1 You are reading a legal document which is the agreement between you, the Customer (whom we refer to as "you", "your", or the "Customer" in this document) and us. All of our services are rendered by Okie Online Store. We are the owner of this website (www.okie.website).</p>
+		<p>1.2 Please read this agreement carefully. By browsing, accessing or using this website or by using any facilities or services made available throught it or by transacting through or on it, you are agreeing to the terms and conditions that appear below (all of which are called the "Agreement"). This Agreement is made between you and us.</p>
+		<p>1.3 These Terms and Conditions were most recently updated on July, 2015 (version 1). We reserve the right to amend these terms and conditions at any time. All amendments to these terms and conditions will be posted on-line. You may terminate this Agreement by written notice to us (by post or by email at okieonlineshop@gmail.com) if you do not wish to be bound by such new terms and conditions. Howerver, continued use of the Service or the Website or a Voucher will be deemed to constitute acceptance of the new terms and conditions.</p>
+		<p>1.4 Your statutory rights: As a consumer, nothing in this Agreement affects your non-excludable statutory rights.</p>
+		<p><br /></p>
+		<p>2. DEFINITIONS</p>
+		<p>2.1 In this Agreement, we use various defined terms. You will know they are defined because they begin with a capital letter. This is what they mean:</p>
+		<p>2.1.1 "Merchant" means a third party seller of goods and services for which you can see via our website.</p>
+		<p>2.1.2 "Microsite" means an auxiliary website supplementary to our main website.</p>
+		<p>2.1.3 "Service" means all or any of the services provided by Okie Online via the Website (or via other electronic or other communication from Okie Online) including the information services, content and transaction capabilities on the Website (including the ability to make a Purchase).</p>
+		<p>2.1.4 "Points" means a point which is subject to terms and conditions, which, if purchased by you, allows you (i.e. the purchaser of it but not anyone else) to redeem it at a particular Merchant in exchange for point offered by that Merchant</p>
+		<p>2.1.5 "Point Redeemable" means goods and/or services offered by a particular Merchant which are described as part of a redeeamble product.</p>
+		<p>2.1.6 "Website" means the "Okie.website" website and any Microsite.</p>
+		<p><br /></p>
+		<p>3. GENERAL ISSUEUS ABOUT THIS WEBSITE AND THE SERVICE</p>
+		<p>3.1 Applicability of terms and conditions: Use of the Service and the Website and any viewers or visitors of our website are each subject to the terms andconditions set out in this Agreement.</p>
+		<p>3.2 Age: The right of age to use the Website and/or the Service (whether with or without registration) and to make any Purchase, you must be 18 years of age or above.</p>
+		<p>3.3 Scope: The Website, Service and any Purchase may not use for your own commercial intentions, personal use only and must not be used for business purposes. For the avoidance of doubt, scraping of the Website (and hacking of the Website) is not allowed.</p>
+		<p>3.4 Preventio on use: We reserve the right to prevent you using the Website and the Service (or any part of them) and to prevent you from making any Purchase.</p>
+		<p>3.5 Maximum ads per advertiser: The maximum ads per advertise is three (3) ads per identified company or individual. Okie Online Store will double check the identity of presence of the advertiser\'s company to protect the user\'s and consumer\'s right.</p>
+		<p><br /></p>
+		<p>4. RESPONSIBLITY: Okie Online Store is;</p>
+		<p>4.1 Okie Online Store will provide information as exact the same announcement as what the companies advertise to their previous ads campaign;</p>
+		<p>4.2 Solely responsible for information which is delivered into the websit, precies and concise from the source, the advertiser.</p>
+		<p>4.3 Solely responsible for any changes that may improved the customer\'s interface and experience, yet may change without prior notice. Any concerns about changes of interface and experience from the customers will be honored but not a totally agreeing.</p>
+		<p>4.4 We are accepting the same industry or field of business, services, or products that may contradict within the Okie Online Store website. The only resposibility of Okie.website is to check its words if legal and moral.</p>
+		<p><br /></p>
+		<p>5. RULES ABOUT USE OF THE SERVICE AND THE WEBSITE</p>
+		<p>5.1 Reading Terms and Conditions: You, as consumers must be responsible for the understanding between us, Okie.website, and you as our consumers. Once you agreed (marked check), on our Terms and Conditions, you are accepting the conditions that were written hereby.</p>
+		<p>5.2 We will use reasonable endeavors to correct any errors or omissions as soon as practicable after being notified of them. However, we do not guarantee that the Service or the Website will be free of faults and we do not accept liability for any errors or omissions. In the event of an error or fault, you should report it by email to: okieonlineshop@gmail.com.</p>
+		<p>5.3 We do not warrant that your use of the Service or the Website will be uninterrupted and we do not warrant that any information (or messages) transmitted via the Service or the Website will be transmitted accurately, reliably, in a timely manner or at all.</p>
+		<p>5.4 We do not give any warranty that the Service or the Website is free from viruses or anything else which may have a harmful effects on any technology.</p>
+		<p>5.5 Also, although we will try to allow uninterrupted access to the Service and the Website, access to the Service and the Website may be suspended, restricted or terminated at any time.</p>
+		<p>5.6 We reserve the right to block access to and/or to edit or remove any material which in our reasonable opinion may give rise to a breah of any of this Agreement.</p>
+		<p><br /></p>
+		<p>6. DATA PROTECTION</p>
+		<p>6.1 Please see our Privacy Policy which forms part of this Agreement</p>
+		<p><br /></p>
+		<p>7. ADVERTISEMENTS</p>
+		<p>7.1 We may place advertisements in different locations on the Website and at different points during use of the Service. These locations and points may change from time to time - but we will always clearly mark which goods and services are advertisements (i.e. from persons other that us), so that it is clear to you which goods and services are provided on an objective basis and which are not (i.e the advertisements).</p>
+		<p>7.2 You are free to select or click on advertised goods and services or not as you see fit.</p>
+		<p>7.3 Any advertisements may be delivered on our behalf by a third party advertising company.</p>
+		<p>7.4 No personal data (for example your name, address, email adress or telephone number) will be used during the course of serving our advertising, but, on our behalf, our third-party advertiser or affiliate may place or recognize a unique "cookie" on your browser (see our Privacy Policy here about this). This cookie will not collect personal data about you nor is it linked to any personal data about you. If you would like to more information about this practice and to know your choices about not having this information used by any company, see our Priavcy Policy here about this which you can click on for more information.</p>
+		<p><br /></p>
+		<p>8. LINKS TO AND FROM OTHER WEBSITES</p>
+		<p>8.1 Where the Website contains links to third party sites and to resources provided by third parties (together "Other Sites"), those Other Sites are merely linked to provide information only and are solely for your convenience. We have no control over and do not accept and we assume no responsibility for Other Sites or fr the content or products or services of Other Sites (including, without limitation, relating to social networking sites such as Facebook) and we accept no responsibility for any loss or damange that may arise from your use of them. If you decide to access any of the third party websites linked to the website, you do so entirely at your own risk.</p>
+		<p>8.2 This (okie.website) website may make available access to Microsites and if it does, it may do so within or otherwise through external hyperlinks.</p>
+		<p><br /></p>
+		<p>9. INTELLECTUAL PROPERTY RIGHTS</p>
+		<p>9.1 All intellectual property rights (including all copyright, patents, trade marks, service marks, trade names, designs (including the "look and feel" and other visual or non-literal elements whether register or unregistered) in the Website and Service, (subject to Clause 9.4) information content on the Website or accessed as part of the Service, any database operated by us and all the website design, text and graphics, software, photos, video, music, sound, and their selection and arragement, and all software compilations, underlying source code and software (including applets and scripts) shal remain our property (or that of our licensors). You shall not, and shall not attempt to, obtain any title to any such intellectual property rights. All rights are reserved.</p>
+		<p>9.2 None of the material listed in Clause 9.1 may be reproduced or redistributed or copied, distributed, republished, downloaded, displayed, posted or transmitted in any form or by any means, sold, rented or sub-licensed, used to create derivative works, or in any way exploited without our prior express written permission. You may, however, retrieve and display the content of the Website on a computer screen, store such content in electronic form on dist (but not on any server or other storage device connected to a network) or priny one copy of such content for your own personal, non-commercial use, provided you keep intact all and any copyright and proprietary notices. You may not otherwise reproduce, modify, copy or distribute or use for commercial purposes any of the materials or content on the Website without our permission.</p>
+		<p>9.3 All rights (including goodwill and, where relevant, trademarks) in the Okie Online are owned by us (or our licensors). Other product and company names mentioned on the Website are the trade marks or registered trade marks of their respective owners.</p>
+		<p>9.4 Title, ownership rights and intellectual property rights in and to the content accessed using the Service is the property of the applicable content owner or Merchant and may be protected by applicable copyright or other law. The Agreement gives you no rights to such content.</p>
+		<p>9.5 The authors of the literary and artistic works in the pages in the Website have asserted their moral rights to be identified as the author of those works.</p>
+		<p>9.6 Subjec to Clause 9.7, any material you transmit or post or submit to the Website (or otherwise to us) shall be considered (and we may treat it as) non-confidential and non-proprietary, subject to our obligations under data protection legislation. If for some reason, any part of that statement does not work as a matter of law, then for anything which you supply to us from whatever source (i.e. via email, copy, modify, adapt, translate, publish and distribute world-wide any such material.</p>
+		<p>9.7 All comments, suggestions, ideas, notes, drawings, concepts or other information: (i) disclosed or offered to us by you; or (ii) in response to solicatations by us regarding the Service or the Website; (in each foregoing case, these are callled "ideas") shall be deemed to be and shall remain our property and you hereby assign by way of present and future assignment all intellectual property rights in ideas, to us. You understand and acknowledge that we have both internal resources and other external which may have developed or may in the future develop ideas identical to or similar to ideas and that we are only willing to consider ideas on these terms. In any event, any ideas are not submitted in confidence and we assume no obligations, express or implied by considering it. Without limitation, we shall exclusively own all now known or herearfter existing rights to the ideas of every kind and nature throughout the world and shall be entitled to unrestricted use of the Ideas for any purpose whatsoever, commercial or otherwise without compensation to the provider of the Ideas.</p>'
 	]
 
 ];
- 
+
